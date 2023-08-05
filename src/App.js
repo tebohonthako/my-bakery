27d86c15be653467da880bab73a155bdb4beb989
T E B O O <tjaynthako@gmail.com>
2023-08-05 23:14:03 +0200

    image added to home page

--- src/App.js
+++ src/App.js
@@ -3,28 +3,17 @@ import './App.css';
 // importing Routes 
 import {Routes, Route} from 'react-router-dom';
 // importing my components or pages 
-import HomePage from './HomePage';
-import Aboutpage from './AboutPage';
-import MenuPage from './MenuPage';
+import Home from './HomePage';
+// import Aboutpage from './AboutPage';
+// import MenuPage from './MenuPage';
 // end of my imported components 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Teboo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home/>
+      {/* <Aboutpage/>
+      <MenuPage/> */}
     </div>
   );
 }
--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 // functional component for Home Page
 function Home() {
-    
+    return (
+        <div>
+            <h1>Home</h1>
+            <img src="https://www.onlinedegree.com/wp-content/uploads/2016/11/master_chef.jpg" alt="master chef at work in the kitchen"/>
+        </div>
+        
+
+    )
 }
 
 
@@ -11,4 +18,4 @@ function Home() {
 
 
 
-export default HomePage;
+export default Home;
