9929caf60475a541664e600be17642cb68218202
T E B O O <tjaynthako@gmail.com>
2023-08-05 23:21:28 +0200

    hero image resized

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,17 +5,14 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <img src="https://www.onlinedegree.com/wp-content/uploads/2016/11/master_chef.jpg" alt="master chef at work in the kitchen"/>
+            <h1>MasterChef Bakery</h1>
+            <h3>Welcome to Master Chef Bakery. The home of world-class pastries!</h3>
+            <img src="https://www.onlinedegree.com/wp-content/uploads/2016/11/master_chef.jpg" alt="master chef at work in the kitchen" width="100%"/>
         </div>
         
 
     )
 }
-
-
-
-
 
 
 export default Home;
