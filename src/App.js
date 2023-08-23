56586d8921f82083d0aea548d854eba59624c0d8
T E B O O <tjaynthako@gmail.com>
2023-08-23 10:25:58 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './HomePage';
 // import MenuPage from './MenuPage';
 
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Home/>
-        <Routes>
-          {/* <Route exact path="/" element={<MenuPage />} />
-          <Route path="/recipe/:id" element={<Products />} /> */}
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+    // <Router>
+    //   <div>
+    //     <Home/>
+    //     <Routes>
+    //       {/* <Route exact path="/" element={<MenuPage />} />
+    //       <Route path="/recipe/:id" element={<Products />} /> */}
+    //     </Routes>
+    //   </div>
+    // </Router>
+  // );
+// }
 
-export default App;
+// export default App;
